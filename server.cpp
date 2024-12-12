42ceab42e257c0662fb40be47ddec728e70364ed
Zarkax <120319524+Zarkax@users.noreply.github.com>
2024-12-12 15:20:33 +0100

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -1,8 +1,3 @@
-#include <arpa/inet.h>
-#include <stdio.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -13,184 +8,217 @@
 #include <signal.h>
 #include <sys/mman.h>
 #include <errno.h>
-#define MAX_PSEUDO_LENGTH 30
-size_t OFFSET = 0; //tracks how much space data occupies in shared memory
-sig_atomic_t trigger_sigint = 0; // tracks if ctrl+C was clicked
-sig_atomic_t PIPE_OK = 0; // tracks if pipes are open
-sig_atomic_t is_manual = 0; // tracks if manuel mode activated
-sig_atomic_t trigger_cleanup = 0; 
-struct sockaddr_in server_address;
+#include <arpa/inet.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <pthread.h>
 
 
+#include "../checked.h"
+#define MAX_CLIENTS 1000
+static volatile sig_atomic_t sigintRecu = 0;
+static volatile sig_atomic_t triggerCleanup = 0;
 
-/**
- * Function to validate a username (pseudo).
- * Ensures it adheres to character and length constraints.
- *
- * @param pseudo The username to validate.
- * @return 0 if valid, error codes otherwise.
- */
-int validate_pseudo(const char *pseudo){
-   if (strlen(pseudo) > MAX_PSEUDO_LENGTH){
-      fprintf(stderr,"Pseudo length exceeded max length\n");
-      exit(2);
-   }
-   if (strpbrk(pseudo,"/-[]")){
-      fprintf(stderr,"invalid character\n");
-      exit(3);
-   }
-   if (strcmp(pseudo, ".") == 0 || strcmp(pseudo, "..") == 0){
-      fprintf(stderr,"invalid character\n");
-      exit(3) ;
-   }
-   
-   return 0;
+
+pthread_mutex_t clients_mutex = PTHREAD_MUTEX_INITIALIZER;  // Mutex pour synchroniser
+
+struct ListeClient {
+    int client_sockets[MAX_CLIENTS];  // Tableau pour stocker les sockets des clients
+    int client_count;             // Nombre de clients connectés
+    char* client_usernames[MAX_CLIENTS];
+};
+
+struct ListeClient liste_client; 
+
+void printClientUsernames() {
+    printf("Liste des usernames des clients :\n");
+    for (int i = 0; i < liste_client.client_count; i++) {
+        printf("Client %d: Username=%s\n", i + 1, liste_client.client_usernames[i]);
+    }
 }
 
-void check_success(int success){
-   if(success <= 0 ){
-      if(success == 0){
-         fprintf(stderr, "Not in presentation format");
-      }
-      else{
-         perror("inet_pton");
-         exit(EXIT_FAILURE);
-      }
-   }
+static void GestionnaireSigint([[ maybe_unused ]] int sigint) {
+   sigintRecu = 1;  // doit tout clean et fermer le programme
+}
+static void GestionnaireSigpipe([[ maybe_unused ]] int sigpipe) {
+    const char *message = "Déconnexion de l'interlocuteur détectée ou Pipe cassé \n";
+    ssize_t bytesWr =  write(STDOUT_FILENO, message, sizeof(message) - 1);
+    (void)bytesWr;
+    triggerCleanup = 1;
+
+}
+void add_client(int client_sock) {
+    pthread_mutex_lock(&clients_mutex);
+    if (liste_client.client_count < MAX_CLIENTS) {
+        liste_client.client_sockets[liste_client.client_count++] = client_sock;
+    
+    } else {
+        printf("Max clients reached. Connection refused.\n");
+        close(client_sock);
+    }
+    pthread_mutex_unlock(&clients_mutex);
 }
 
-int create_socket(){
-   int client_fd;
-   //initialise socket
-   if((client_fd = socket(AF_INET,SOCK_STREAM,0)) < 0){
-      perror("socket failed");
-      exit(EXIT_FAILURE);
-   }
-
-   // initialise the address + port number
-   server_address.sin_family = AF_INET;
-   server_address.sin_port = htons(1234);
-   int success;
-   success = inet_pton(AF_INET ,"127.0.0.1", &server_address.sin_addr);
-   check_success(success); // checks if inet_pton worked as intended
-
-   char* addr_variable_value ;
-   addr_variable_value = getenv("IP_SERVEUR");
-   if(addr_variable_value != NULL){ //change the address 
-      success = inet_pton(AF_INET,addr_variable_value,&server_address.sin_addr);
-      check_success(success);
-   }
-
-   const char* port_value = getenv("PORT_SERVEUR"); // retrieve the value if the variable exists
-   if(port_value != NULL ){ //IP_SERVER is defined
-      int value;
-      value = atoi(port_value);
-      if (value >= 1 && value <= 65535) {
-         server_address.sin_port = htons(value);
-      }
-   }
-
-   return client_fd;
+bool no_space(const char * buffer){
+    for (int i = 0; buffer[i] != '\0'; i++) {
+        if (buffer[i] == ' '){
+            return false;
+        }
+    }
+    return true;
 }
 
-void display_messages(char* shared_memory){
-    printf("%s",shared_memory);
-    fflush(stdout);
-    memset(shared_memory, 0, 1024); // Clear 1024 bytes of shared memory
-    OFFSET = 0;
-}
-
-
-void* send_messages(int socket_fd, const char* username, int isbot,int ismanual,char* shared_memory){
-    //retrieve message
-    char message[1055]; //size sender's username
-
-    while(1){
-            // Read user input
-            ssize_t n = read(STDIN_FILENO, message, sizeof(message) - 1);
-            if (n > 0) {
-               message[n - 1] = '\0'; // Remove newline
-            } else if (n == -1 && errno == EINTR) {
-                // Interrupted by signal, continue
-                continue;
-
-            } else if (n == 0) {  // termine le programme en retournant 0
-                perror("stdin fermé ou EOF atteint");
-                exit(0);
-
-            }
-             else if (n < 0) {
-                perror("Erreur lors de la lecture");
+bool add_username(char * buffer){
+    pthread_mutex_lock(&clients_mutex);
+    if (liste_client.client_count < MAX_CLIENTS){
+        if (no_space(buffer)){
+            liste_client.client_usernames[liste_client.client_count++] = strdup(buffer); // alloue la memoire
+            if (!liste_client.client_usernames[liste_client.client_count - 1]) {
+                perror("Allocation mémoire échouée");
                 exit(EXIT_FAILURE);
-            }           
-
-
-
-
-            ssize_t success = write(socket_fd, message, strlen(message) + 1);
-            if(!isbot){
-                printf("[\x1B[4m%s\x1B[0m] %s\n", username, message);
-                fflush(stdout);
+        
             }
-            if (success == -1){
-                    perror("Erreur lors de l'écriture dans le pipe");
-                    close(socket_fd);
-                    exit(EXIT_FAILURE);
-            }
-            if (ismanual == 1 && !isbot){
-                display_messages(shared_memory);
-            }
-     }
+            printClientUsernames();
+            pthread_mutex_unlock(&clients_mutex);
+            return true;
+        }
+    }
+    else{
+        pthread_mutex_unlock(&clients_mutex);
+        return false;
+    }
+    
+}
 
-        close(socket_fd);
+char* get_username(char *buffer) {
+    char *username = malloc(20); // Allouer dynamiquement
+    if (!username) {
+        perror("Erreur malloc \n");
+        exit(EXIT_FAILURE);
+    }
+    int i = 0;
+    while (buffer[i] != '\0' && buffer[i] != ' ') {
+        username[i] = buffer[i];
+        i++;
+    }
+    username[i] = '\0';
+    return username;
 }
 
 
-int main(int argc, char* argv[]) {
-   //not enough arguments
-    if (argc < 2) {
-        fprintf(stderr, "chat pseudo_utilisateur pseudo_destinataire [--bot] [--manuel]\n");
-        return 1;
-   }
+void *handle_client(void *client_sock) {
+    int sock = *(int *)client_sock;
+    free(client_sock); // client_sock est inutile
 
-   char *username = argv[1];
-   validate_pseudo(username);
+    char buffer[1024];
+    while (1) {
+        int bytes_read = read(sock, buffer, sizeof(buffer));
+        if (bytes_read > 0) {
+            pthread_mutex_lock(&clients_mutex);
+            if(!add_username(buffer)){   // ajoute un username dans la liste
+                // envoie un msg au client si le buffer n'est pas un username
+                char username[20] = get_username(buffer);
+                for (int i = 0; i < liste_client.client_count; i++){
+                    if (strcmp(username, liste_client.client_usernames[i]) == 0){ // verifie si le username est présent
+                        write(liste_client.client_sockets[i], buffer, strlen(buffer)); 
+                        // envoie un message a la bonne personne 
+                    }
+                }
+            }
 
-   int is_bot = 0;
-   char* shared_memory = NULL;
-   for (int i = 2; i < argc; i++) {
-      if (strcmp(argv[i], "--bot") == 0) {
-         is_bot = 1;
-      }
-      else if (strcmp(argv[i], "--manuel") == 0) {
-         is_manual = 1;
+            printf(" %s\n", buffer);
+            send(sock, "Message received!", strlen("Message received!"), 0);
+            
 
-         //create shared memory
-         const int protection = PROT_READ | PROT_WRITE;
-         const int visibility = MAP_SHARED | MAP_ANONYMOUS;
-         shared_memory = (char*)mmap(NULL, 4096,protection, visibility, -1, 0);
+            pthread_mutex_unlock(&clients_mutex);
+        }
+        else if (bytes_read == 0) { // Le client a fermé la connexion
+                printf("Client disconnected: socket %d\n", sock);
+                close(sock);
+                pthread_mutex_lock(&clients_mutex);
+                for (int i = 0; i < liste_client.client_count; i++) {
+                    if (liste_client.client_sockets[i] == sock) {
+                        liste_client.client_sockets[i] = liste_client.client_sockets[--liste_client.client_count];
+                        free(liste_client.client_usernames[i]); // Libère la mémoire allouée pour le username
+                        break;
+                    }
+                }
+                pthread_mutex_unlock(&clients_mutex);
+        }
+        else if (bytes_read < 0){
+            perror("erreur lord de la lecture");
+            // faire fonction cleanup
+        }
+        
+    }
+    close(sock);
+    return NULL;
+}
 
-         if(shared_memory == MAP_FAILED ){
-               perror("mmap");
-               exit(EXIT_FAILURE);
-         }
-      }
-      else{
-         fprintf(stderr, "chat pseudo_utilisateur pseudo_destinataire [--bot] [--manuel]\n");
-         return -1;
-      }
-   }
+void sock_creation(){
+    const char *port_str = getenv("PORT_SERVEUR");
+    int port = 1234; // Default port
+    if (port_str != NULL) {
+        port = atoi(port_str); // Convert to integer
+        if (port < 1 || port > 65535) {
+            port = 1234; // Reset to default if out of range
+        }
+    }
+    liste_client.client_count = 0; // initialise à 0
 
-   int client_fd; //create socket 
-   client_fd = create_socket();
+    int server_fd = checked(socket (AF_INET , SOCK_STREAM , 0)); // Créer le socket
 
-   // Connect client to server
-   if(connect(client_fd, (struct sockaddr *)&server_address, sizeof(server_address)) < 0) {
-        perror("Connect failed");
-        close(client_fd);
-        return EXIT_FAILURE;
-   }
-   send_messages(client_fd,username,is_bot,is_manual,shared_memory);
-   // create a thread to read from socket
-   return 0;
+    int opt = 1;
+    // Permet la réutilisation du port/de l'adresse
+    setsockopt (server_fd , SOL_SOCKET , SO_REUSEADDR | SO_REUSEPORT , &opt , sizeof (opt ));
+    struct sockaddr_in address ;
+    address . sin_family = AF_INET ;
+    address . sin_addr .s_addr = INADDR_ANY ;
+    address . sin_port = htons (port);
+    fflush(stdout);
+    // Définit l'adresse et le port d'écoute , réserve le port
+    checked(bind(server_fd , ( struct sockaddr *)& address , sizeof ( address )));
+
+    // Commence l'écoute
+    checked(listen (server_fd , 5)); // maximum 3 connexions en attente
+    size_t addrlen = sizeof ( address );
+    // Ouvre une nouvelle connexion
+
+
+    while (1) {
+        int new_socket = checked(accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen));
+
+        if (new_socket >= 0) {
+            int *client_sock = (int*)malloc(sizeof(int));
+            if (client_sock == NULL) {
+                perror("malloc failed");
+                close(new_socket);
+                break;
+            }
+            add_client(new_socket);
+            *client_sock = new_socket;
+            pthread_t tid;
+            pthread_create(&tid, NULL, handle_client, client_sock);
+            pthread_detach(tid); // Automatically free thread resources
+        }
+
+    }
+   close(server_fd);
+
+}
+
+int main(void) {
+    signal(SIGINT, GestionnaireSigint);
+    signal(SIGPIPE, GestionnaireSigpipe);
+    sock_creation();
+    return 0;
+
 }
